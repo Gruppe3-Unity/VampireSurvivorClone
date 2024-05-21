3e2d42945aa7d1aec774660a036c48d2317a609b
MrHexeberg <147028840+MrHexeberg@users.noreply.github.com>
2024-05-21 14:15:30 +0200

    1.Spiller Bevegelse

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,5 +8,19 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public float MoveSpeed;
+    float SpeedX, SpeedY;
+    Rigidbody2D MyRigidBody;
 
+    void Start (){
+        MyRigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update(){
+        SpeedX = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+        SpeedY = Input.GetAxisRaw("Vertical") * MoveSpeed;
+        MyRigidBody.velocity = new Vector2(SpeedX,SpeedY);
+    }
+  
 }
