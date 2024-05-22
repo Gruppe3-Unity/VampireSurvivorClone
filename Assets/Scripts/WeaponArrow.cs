7aad1d2039c94ae637362f6e8a9b5c48db719b94
MrHexeberg <143922@usn.no>
2024-05-22 18:19:26 +0200

    fix the arrow's
    so now the arrow spawner flows the player, and the arrow do damage and the arrow fire in the deration of travel, and the arrows are rotated in the deration they are traveling in

--- Assets/Scripts/WeaponArrow.cs
+++ Assets/Scripts/WeaponArrow.cs
@@ -11,8 +11,19 @@ public class WeaponArrow : Weapon
     [SerializeField]
     private float arrowSpeed = 20f;
 
+     public Transform Player;
+       public Rigidbody2D Playerbody;
+
+    void Start(){
+        Player = FindObjectOfType<Player>().transform;
+        Playerbody = Player.GetComponent<Rigidbody2D>();
+    }
+
+
     private void Update()
     {
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ShootArrow();
@@ -21,17 +32,31 @@ public class WeaponArrow : Weapon
 
     private void ShootArrow()
     {
+       
+
         // Instantiate the arrow at the character's position and rotation
-        GameObject arrowInstance = Instantiate(arrowPrefab, transform.position, transform.rotation);
+        GameObject arrowInstance = Instantiate(arrowPrefab, transform.position,transform.rotation);
 
         // Get the Rigidbody2D component from the arrow instance
         Rigidbody2D arrowRigidbody = arrowInstance.GetComponent<Rigidbody2D>();
 
         // Calculate the direction the arrow should move
-        Vector2 arrowDirection = transform.right * arrowSpeed;
+        Vector2 arrowDirection;
+        if (Playerbody.velocity.normalized == Vector2.zero){
+
+            arrowDirection = transform.right * arrowSpeed;
+        }
+        else{
+            arrowDirection = Playerbody.velocity.normalized * arrowSpeed;
+        }
 
         // Apply a force to the arrow to move it in the calculated direction
         arrowRigidbody.AddForce(arrowDirection, ForceMode2D.Impulse);
+
+        float ArrowAngel = Vector2.SignedAngle(Vector2.right,arrowDirection.normalized);
+
+        arrowInstance.transform.Rotate(0,0,ArrowAngel);
+
 
         // Destroy the arrow after 2 seconds
         Destroy(arrowInstance, 2f);
